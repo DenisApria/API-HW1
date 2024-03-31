4e8acb497efd672538e8e87eefd6245eaa21e315
DenisApria <151721167+DenisApria@users.noreply.github.com>
2024-03-31 18:20:54 +0400

    Update CityRepository.java

--- src/main/java/ge/ibsu/demo/repositories/CityRepository.java
+++ src/main/java/ge/ibsu/demo/repositories/CityRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
 
-    @Query("SELECT new ge.ibsu.demo.dto.CityInfo(c.city, c.country.country) FROM city c")
+    @Query("SELECT new ge.ibsu.demo.dto.CityInfo(c.city, c.country.country) FROM City c")
     List<CityInfo> getAllCitiesWithCountry();
 }
